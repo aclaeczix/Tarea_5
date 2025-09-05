94e8f18b81d436c3e2709b9fa835511a0856a59c
aclaeczix <alyaalme12@yahoo.com>
2025-09-04 20:57:17 -0600

    Update queue.ts

--- queue/queue.ts
+++ queue/queue.ts
@@ -1,6 +1,6 @@
 import { Node } from "./node";
 
-class Queue {
+export class Queue {
   head: Node | null;
   tail: Node | null;
 
@@ -9,8 +9,42 @@ class Queue {
     this.tail = null;
   }
 
-  enqueue(value: number) {}  
-  dequeue(): Node {}
-  
-  list(): void {}
+  // Agregar un valor al final de la cola
+  enqueue(value: number): void {
+    const nuevo = new Node(value);
+
+    if (this.tail === null) {
+      // Si está vacía, head y tail son el mismo
+      this.head = nuevo;
+      this.tail = nuevo;
+    } else {
+      this.tail.next = nuevo;
+      this.tail = nuevo;
+    }
+  }
+
+  // Sacar el primer valor de la cola
+  dequeue(): number | null {
+    if (this.head === null) {
+      return null; // Cola vacía
+    }
+
+    const valor = this.head.value;
+    this.head = this.head.next;
+
+    if (this.head === null) {
+      this.tail = null; // Si quedó vacía
+    }
+
+    return valor;
+  }
+
+  // Mostrar todos los elementos
+  list(): void {
+    let actual = this.head;
+    while (actual !== null) {
+      console.log(actual.value);
+      actual = actual.next;
+    }
+  }
 }
